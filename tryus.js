71f8997e394ecc2ce1df0b06f415b4909ebfb867
HaninHaidrah <>
2021-06-16 15:47:01 +0000

    to add js

--- tryus.js
+++ tryus.js
@@ -4,11 +4,18 @@
 
 var answer = prompt("Are you intrested in Arab food",);
 
-if (answer == "yes" ) {
+if (answer == "yes") {
   alert  ("welcome its the right place");
-} else {
-  alert ("Hello"  "! Its not a good one for you?");
 }
-console.log(answer)
+else {
+  alert ("Hello ! Its not a good one for you?");
+}
+
+ 
+
+
+        
+
+
 
 
