0a280f68e41507923c2e6bc37614742dc631a0ea
HaninHaidrah <>
2021-06-16 15:38:03 +0000

    addition

--- tryus.js
+++ tryus.js
@@ -4,11 +4,17 @@
 
 var answer = prompt("Are you intrested in Arab food",);
 
-if (answer == "yes" ) {
+if (answer == "yes") {
   alert  ("welcome its the right place");
 } else {
   alert ("Hello"  "! Its not a good one for you?");
 }
-console.log(answer)
+
+ 
+
+
+        
+
+
 
 
