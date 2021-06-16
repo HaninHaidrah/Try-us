48d105a4e9de383ccd91975cdf9d8e9139925560
HaninHaidrah <>
2021-06-16 14:09:25 +0000

    to gunction it

---
+++ tryus.js
@@ -0,0 +1,14 @@
+
+
+
+
+var answer = prompt("Are you intrested in Arab food",);
+
+if (answer == "yes" ) {
+  alert  ("welcome its the right place");
+} else {
+  alert ("Hello"  "! Its not a good one for you?");
+}
+console.log(answer)
+
+
