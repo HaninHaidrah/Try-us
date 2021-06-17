0bc812e2a60994f75918a2c3996cca9fce502f88
HaninHaidrah <>
2021-06-17 13:56:39 +0000

    change

---
+++ app.js
@@ -0,0 +1,42 @@
+
+ 
+
+
+
+
+//  var food = prompt ("what is your favourite food?")
+
+// alert ("welcome to Arab recipes website " + "you can downlod stuffs after sign up")
+
+
+// var answer = prompt ("Are you intrested in Arab food");
+
+// while (answer !== "yes" && answer !== "Yes" && answer !== "YES" ){ 
+//   alert ("Sorry ! Its not a good one for you")
+
+//   answer = prompt ("Are you intrested in Arab food");
+//   }
+
+
+//   alert("welcome its the right place")
+
+
+
+//  var numberOfrecipes = prompt("how many recipes you want to see each time")
+
+//   for(var i =1 ; i<=numberOfrecipes ; i++)
+//   {
+
+// document.write('<img src="https://i.ytimg.com/vi/U3dNHep8SLQ/maxresdefault.jpg" width="400" height="200">')
+
+//   }
+
+
+ 
+
+
+        
+
+
+
+
