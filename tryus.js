673a5f225cd0cbd3876f43b793d923eee6f917b1
HaninHaidrah <>
2021-06-17 08:26:53 +0000

    rechange

--- tryus.js
+++ tryus.js
@@ -1,18 +1,27 @@
 
 
 
+var food = prompt ("what is your favourite food?")
+
+alert ("welcome to Arab recipes website " + "you can downlod stuffs after sign up")
+
 
 var answer = prompt("Are you intrested in Arab food",);
 
 if (answer == "yes") {
   alert  ("welcome its the right place");
 }
-<<<<<<< HEAD
+
 else {
   alert ("Hello ! Its not a good one for you?");
+
 }
-=======
->>>>>>> 0a280f68e41507923c2e6bc37614742dc631a0ea
+  
+  
+document.write( "<h4 style="text-align:center">Who We Are:</h4><p>we are chefs from aroun Arab words who like to share our tradtional recipes with you</p>")
+ 
+
+
 
  
 
