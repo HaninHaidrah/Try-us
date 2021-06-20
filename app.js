e590d216e835f9c32dfaef295ffd48dcd5b64109
HaninHaidrah <>
2021-06-20 09:31:41 +0000

    change

--- app.js
+++ app.js
@@ -4,32 +4,36 @@
 
 
 
-//  var food = prompt ("what is your favourite food?")
+ var food = prompt ("what is your favourite food?")
 
-// alert ("welcome to Arab recipes website " + "you can downlod stuffs after sign up")
+alert ("welcome to Arab recipes website " + "you can downlod stuffs after sign up")
+
+function Arabicrecipes(){
+var answer = prompt ("Are you intrested in Arab food");
+
+while (answer !== "yes" && answer !== "Yes" && answer !== "YES" ){ 
+  alert ("Sorry ! Its not a good one for you")
+
+  answer = prompt ("Are you intrested in Arab food");
+  }
+
+}
+
+Arabicrecipes();
+  alert("welcome its the right place")
 
 
-// var answer = prompt ("Are you intrested in Arab food");
 
-// while (answer !== "yes" && answer !== "Yes" && answer !== "YES" ){ 
-//   alert ("Sorry ! Its not a good one for you")
+ var numberOfrecipes = prompt("how many recipes you want to see each time")
 
-//   answer = prompt ("Are you intrested in Arab food");
-//   }
+function numrecip(){
+  for(var i =1 ; i<=numberOfrecipes ; i++)
+  {
 
+document.write('<img src="https://i.ytimg.com/vi/U3dNHep8SLQ/maxresdefault.jpg" width="400" height="200">')
 
-//   alert("welcome its the right place")
-
-
-
-//  var numberOfrecipes = prompt("how many recipes you want to see each time")
-
-//   for(var i =1 ; i<=numberOfrecipes ; i++)
-//   {
-
-// document.write('<img src="https://i.ytimg.com/vi/U3dNHep8SLQ/maxresdefault.jpg" width="400" height="200">')
-
-//   }
+  }
+}
 
 
  
